8fc090ef9a6b1fd72e7a3697f261ff4a730085e1
David Kuenka <cuencaldd2001.29@gmail.com>
2022-02-19 02:01:06 +0300

    DJ SECRET KEY, Allowed host backend, DEBUG False

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -7,11 +7,11 @@ load_dotenv()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [os.getenv('HOST')]
+ALLOWED_HOSTS = [os.getenv('HOST'), 'backend']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
